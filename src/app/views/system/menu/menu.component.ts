169a0dc2eeabd6c71a59515cace0d2ac10d890e4
m310851010 <310851010@qq.com>
2023-12-08 18:55:06 +0800

    增加布局和样式

--- src/app/views/system/menu/menu.component.ts
+++ src/app/views/system/menu/menu.component.ts
@@ -1,11 +1,44 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { FormsModule } from '@angular/forms';
+import { NzTreeModule, NzTreeNodeOptions } from 'ng-zorro-antd/tree';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'ma-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NzFormModule, NzCardModule, NzInputModule, FormsModule, NzTreeModule, NzButtonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.less'
 })
-export default class MenuComponent {}
+export default class MenuComponent {
+  searchText = '';
+  nodes: NzTreeNodeOptions[] = [
+    {
+      title: 'parent 1',
+      key: '100',
+      children: [
+        {
+          title: 'parent 1-0',
+          key: '1001',
+          disabled: true,
+          children: [
+            { title: 'leaf 1-0-0', key: '10010', disableCheckbox: true, isLeaf: true },
+            { title: 'leaf 1-0-1', key: '10011', isLeaf: true }
+          ]
+        },
+        {
+          title: 'parent 1-1',
+          key: '1002',
+          children: [
+            { title: 'leaf 1-1-0', key: '10020', isLeaf: true },
+            { title: 'leaf 1-1-1', key: '10021', isLeaf: true }
+          ]
+        }
+      ]
+    }
+  ];
+}
--- src/app/views/system/user/user.component.ts
+++ src/app/views/system/user/user.component.ts
@@ -1,11 +1,82 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NzxLayoutPageModule } from '@xmagic/nzx-antd/layout-page';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormlyFieldConfig, FormlyModule } from '@ngx-formly/core';
+import { NzField, NzFormlyModule } from '@xmagic/nz-formly';
+import { FormlyNzInputModule } from '@xmagic/nz-formly/input';
+import { FormlyNzFormFieldModule } from '@xmagic/nz-formly/field-wrapper';
+import { FormlyNzGridModule } from '@xmagic/nz-formly/grid';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { FormlyNzButtonModule } from '@xmagic/nz-formly/button';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
 
 @Component({
   selector: 'ma-user',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    NzxLayoutPageModule,
+    ReactiveFormsModule,
+    NzFormlyModule,
+    FormlyNzInputModule,
+    FormlyNzFormFieldModule,
+    FormlyNzGridModule,
+    FormlyNzButtonModule,
+    FormlyModule,
+    NzFormModule,
+    NzButtonModule,
+    NzSpaceModule
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.less'
 })
-export default class UserComponent {}
+export default class UserComponent {
+  gridOptions = { colNzMd: { span: 12 }, colNzXl: { span: 6 }, colNzXs: { span: 24 }, nzGutter: 15 };
+  searchForm = new FormGroup({});
+  searchModal = {};
+  searchFields: FormlyFieldConfig[] = [
+    {
+      type: 'row',
+      props: { nzGutter: 15 },
+      fieldGroup: [
+        {
+          type: 'input',
+          props: {
+            label: '用户名',
+            ...this.gridOptions
+          }
+        },
+        {
+          type: 'input',
+          props: {
+            label: '用户名',
+            ...this.gridOptions
+          }
+        },
+        {
+          type: 'row',
+          props: {
+            nzGutter: 10
+          },
+          fieldGroup: [
+            {
+              type: 'button',
+              props: {
+                text: '查询',
+                nzType: 'primary'
+              }
+            },
+            {
+              type: 'button',
+              props: {
+                text: '重置'
+              }
+            }
+          ]
+        }
+      ]
+    }
+  ];
+}
