505b0c05c2eb94a8b5ae26f67b6cfe75f1f2f9c8
m310851010 <310851010@qq.com>
2023-12-18 00:14:27 +0800

    字典实体

--- src/app/views/system/dic/dic.component.ts
+++ src/app/views/system/dic/dic.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+import { DicService } from '@xmagic/nzx-antd/service';
+
 @Component({
   selector: 'ma-dic',
   standalone: true,
@@ -8,4 +10,50 @@ import { Component } from '@angular/core';
   templateUrl: './dic.component.html',
   styleUrl: './dic.component.less'
 })
-export default class DicComponent {}
+export default class DicComponent {
+  constructor() {}
+}
+
+interface Dict {
+  id: string;
+  /**
+   * 父节点
+   */
+  pid: string;
+  /**
+   * 名称
+   */
+  label: string;
+  /**
+   * 字典值
+   */
+  value: string | number;
+  /**
+   * 字典类型：0系统类，1业务类
+   */
+  dictType: 0 | 1;
+  /**
+   * 排序号
+   */
+  sort: number;
+  /**
+   * 数据类型,0:string 0: number
+   */
+  dataType: 0 | 1;
+  /**
+   * 删除标识：0未删除，1已删除
+   */
+  isDel: 0 | 1;
+  /**
+   * 状态 0: 正常 1: 停用
+   */
+  status: 0 | 1;
+  /**
+   * 扩展JSON数据
+   */
+  extJson?: string;
+  /**
+   * 描述
+   */
+  desc?: string;
+}
